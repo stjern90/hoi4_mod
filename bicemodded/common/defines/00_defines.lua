0f6be025d774193a37438b1186300bf7802de963
Johan Stjern <johan.stjern90@gmail.com>
2017-01-31 23:09:09 +0100

    Armor and Piercing values calculation changed.
    Fixed French air manu.

--- bicemodded/common/defines/00_defines.lua
+++ bicemodded/common/defines/00_defines.lua
@@ -502,8 +502,8 @@ NMilitary = {
 	STRATEGIC_REDEPLOY_ORG_RATIO = 0.3,				-- Ratio of max org while strategic redeployment
 	BATALION_NOT_CHANGED_EXPERIENCE_DROP = 0.0,		-- Division experience drop if unit has same batalion
 	BATALION_CHANGED_EXPERIENCE_DROP = 0.5,			-- Division experience drop if unit has different batalion
-	ARMOR_VS_AVERAGE = 0.3,			                -- how to weight in highest armor & pen vs the division average
-	PEN_VS_AVERAGE = 0.4,
+	ARMOR_VS_AVERAGE = 0.6,			                -- how to weight in highest armor & pen vs the division average
+	PEN_VS_AVERAGE = 0.6,
 
 	DECRYPTION_ADVANTAGE_BONUS = 0.04,				-- How much unit base damage and base defense is increased per decryption level compared to enemies average encryption level (ex: dec. level is 5 and enemy average enc. level is 3 then 5-3 = 2 which gives 10 % increase)
 	
