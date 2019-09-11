d0ae58475412e96eb6f7ad71e136ca067caf3944
tahlaskerssen <35597835+tahlaskerssen@users.noreply.github.com>
2019-09-11 11:34:14 -0300

    fixx2

--- bicemodded/common/defines/00_graphics.lua
+++ bicemodded/common/defines/00_graphics.lua
@@ -1,4 +1,4 @@
-NDefines_Graphics = {
+﻿NDefines_Graphics = {
 
 NWiki = {
 	BASE_URL = "http://www.hoi4wiki.com/",
@@ -672,7 +672,7 @@ NGraphics = {
 	COUNTRY_FLAG_STRIPE_TEX_MAX_HEIGHT = 4102,
 	COUNTRY_FLAG_LARGE_STRIPE_MAX_WIDTH = 41,
 	COUNTRY_FLAG_LARGE_STRIPE_MAX_HEIGHT = 12000,
-	VICTORY_POINT_LEVELS = 3,												--2 --Number of city icons, capital not included
+	VICTORY_POINT_LEVELS = 2,												--2 --Number of city icons, capital not included
 	VICTORY_POINT_MAP_ICON_AFTER = {0, 2, 3, 10},							-- {0, 20}, -- After this amount of VP the map icon becomes bigger dot.
 	VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {150, 250, 500, 500},  			-- At what camera distance the VP name text disappears.
 	VICTORY_POINTS_DISTANCE_CUTOFF = {150, 250, 500, 500},					-- {250, 500, 1000}, 	-- At what distance VPs are hidden
@@ -701,21 +701,9 @@ NGraphics = {
 	NAVAL_MINES_CLUMP_NEAR_TERRITORY = 25, -- Higher chance to spawn 3d naval mine near our territory
 	NAVAL_MINES_COUNT_TO_VISUAL_ASPECT = 0.1, -- How many in-game-naval-mines is one visual 3d naval mine?
 	MAP_ICONS_GROUP_MAX_SIZE = 15, -- max size in screen pixels of the group of merged icons
-<<<<<<< HEAD
-<<<<<<< HEAD
 	MAP_ICONS_GROUP_CAM_DISTANCE = 165.0, -- camera distance at which the icons begin to group up
 	MAP_ICONS_STATE_GROUP_CAM_DISTANCE = 500.0, -- Camera distance at which the icons begin to group up on state level
 	MAP_ICONS_STRATEGIC_GROUP_CAM_DISTANCE = 700, -- second camera distance at which the icons begin to group up
-=======
-	MAP_ICONS_GROUP_CAM_DISTANCE = 160.0, -- camera distance at which the icons begin to group up
-	MAP_ICONS_STATE_GROUP_CAM_DISTANCE = 450.0, -- Camera distance at which the icons begin to group up on state level
-	MAP_ICONS_STRATEGIC_GROUP_CAM_DISTANCE = 675, -- second camera distance at which the icons begin to group up
->>>>>>> f672edabfd6c5c8a2384828866d721bd41e25b19
-=======
-	MAP_ICONS_GROUP_CAM_DISTANCE = 165.0, -- camera distance at which the icons begin to group up
-	MAP_ICONS_STATE_GROUP_CAM_DISTANCE = 500.0, -- Camera distance at which the icons begin to group up on state level
-	MAP_ICONS_STRATEGIC_GROUP_CAM_DISTANCE = 700, -- second camera distance at which the icons begin to group up
->>>>>>> 5caac7476a0c709e90f65145dec0505965ad8f79
 	MAP_ICONS_STRATEGIC_AREA_HUGE = 220,
 	MAP_ICONS_STATE_HUGE = 100,
 	MAPICON_GROUP_PASSES = 20, -- how many mapicons get processed per frame for grouping. more = quicker response, fewer = better performance
