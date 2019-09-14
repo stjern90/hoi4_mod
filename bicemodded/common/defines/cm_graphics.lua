394608d4b5ea62a0212b25a837deb1fa07b7dd4a
Johan <johan.stjern90@gmail.com>
2019-09-14 18:04:01 +0200

    naval stuff

--- bicemodded/common/defines/cm_graphics.lua
+++ bicemodded/common/defines/cm_graphics.lua
@@ -8,8 +8,6 @@
 
 NDefines_Graphics.NMapIcons.STRATEGIC_AIR_PRIORITY_AIR_MISSION = 290
 
-NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {300, 500, 1500}
-
 --NDefines_Graphics.NGraphics.MAP_ICONS_GROUP_MAX_SIZE = 0
 NDefines_Graphics.NGraphics.MAP_ICONS_GROUP_CAM_DISTANCE = 150				--group moving and still units
 NDefines_Graphics.NGraphics.MAP_ICONS_STATE_GROUP_CAM_DISTANCE = 400.0		--group into states
