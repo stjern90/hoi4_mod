d39eb149ed14464b65f66c90a782630f69b876e8
Johan <johan.stjern90@gmail.com>
2021-01-08 00:38:07 +0100

    merge from steam branch work on raj, saf, germany and free france.
    tried to make free france do the coups in the colonies.

--- bicemodded/common/defines/00_defines.lua
+++ bicemodded/common/defines/00_defines.lua
@@ -932,7 +932,7 @@ NMilitary = {
 	OUT_OF_FUEL_EQUIPMENT_MULT = 0.5,				-- ratio of the stats that you get from equipments that uses fuel and you lack it
 	OUT_OF_FUEL_SPEED_MULT = 0.8,					-- speed mult that armies get when out of fuel
 	OUT_OF_FUEL_TRAINING_XP_GAIN_MULT = 0.0,		-- xp gain mult from training when a unit is out of fuel
-	FUEL_CAPACITY_DEFAULT_HOURS = 196             -- default capacity if not specified
+	FUEL_CAPACITY_DEFAULT_HOURS = 192             -- default capacity if not specified
 },
 
 
