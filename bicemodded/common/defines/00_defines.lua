5964d444090035a932af501a03e1541152670b0d
Johan <johan.stjern90@gmail.com>
2021-03-30 22:56:09 +0200

    ships org normalization, max offset in org/hp ratio reduced Dont remember why we added the hit profile modifier to submarines? incoming air attacks? not sure

--- bicemodded/common/defines/00_defines.lua
+++ bicemodded/common/defines/00_defines.lua
@@ -1545,7 +1545,7 @@ NNavy = {
 	
 	DEPTH_CHARGES_HIT_CHANCE_MULT 									= 2.5, 		-- multiplies hit chance of small guns
 	DEPTH_CHARGES_DAMAGE_MULT 										= 1.0, 		-- multiplies damage of depth charges
-	DEPTH_CHARGES_HIT_PROFILE 										= 100.0,	-- hit profile for depth charges
+	DEPTH_CHARGES_HIT_PROFILE 										= 60.0,	-- hit profile for depth charges
 	
 	
 	CONVOY_HIT_PROFILE												= 103.0,  	-- convoys has this contant hit profile
