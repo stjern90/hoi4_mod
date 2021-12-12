b514ba90f5a4e5c7f6c28e8a8dd98cc3af5b70f5
Blood <leo.thuring03@gmail.com>
2021-12-12 02:02:55 +0100

    germany rail focus change less trucks for supply hubs

--- bicemodded/common/defines/00_defines.lua
+++ bicemodded/common/defines/00_defines.lua
@@ -3447,7 +3447,7 @@ NSupply = {
 	-- The range bonus added to a fully motorized hub. This supply is added on top of the XXX_INITIAL_SUPPLY_FLOW defined above.
 	SUPPLY_HUB_FULL_MOTORIZATION_BONUS = 2.2,
 	-- How many trucks does it cost to fully motorize a hub
-	SUPPLY_HUB_FULL_MOTORIZATION_TRUCK_COST = 150.0,
+	SUPPLY_HUB_FULL_MOTORIZATION_TRUCK_COST = 100.0,
 	-- For each additional level of motorization on a hub (i.e. contry with set motoriazation) reduce max bonus for next level by this amount
 	SUPPLY_HUB_MOTORIZATION_MARGINAL_EFFECT_DECAY = 1.6,
 
