b8ee4c569e54e6a7f43870168e943aa169eaad3d
Johan <johan.stjern90@gmail.com>
2023-10-24 22:40:30 +0200

    industrial org fixes

--- bicemodded/common/defines/00_defines.lua
+++ bicemodded/common/defines/00_defines.lua
@@ -801,7 +801,7 @@ NMilitary = {
 	
 	MAX_DIVISION_BRIGADE_WIDTH = 5,			-- Max width of regiments in division designer.
 	MAX_DIVISION_BRIGADE_HEIGHT = 4,		-- Max height of regiments in division designer.
-	MIN_DIVISION_BRIGADE_HEIGHT = 1,		-- Min height of regiments in division designer.
+	MIN_DIVISION_BRIGADE_HEIGHT = 4,		-- Min height of regiments in division designer.
 	MAX_DIVISION_SUPPORT_WIDTH = 2,			-- Max width of support in division designer.
 	MAX_DIVISION_SUPPORT_HEIGHT = 4,		-- Max height of support in division designer.
 	
